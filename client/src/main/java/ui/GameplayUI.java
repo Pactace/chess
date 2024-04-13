6feabe4bc2bac23e3903b33126b0e6d2a6ce8e6c
Pactace <iantheartist01@gmail.com>
2024-04-13 13:29:13 -0600

    got the chess position conversions to work

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -13,14 +13,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.Math.abs;
+import static ui.BoardCreation.boardCreation;
+import static ui.BoardCreation.setBoard;
 import static ui.EscapeSequences.*;
 
 public class GameplayUI {
-
-    private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_CHARS = 1;
-    private static final String EMPTY = "   ";
-    private static boolean tileColor = true;
     private final NavigatorUI navigator;
     private final ServerFacade serverFacade;
     private final int gameID;
@@ -48,6 +45,7 @@ public class GameplayUI {
             board = game.getBoard();
             board.resetBoard();
         }
+        setBoard(board);
         boardCreation(color);
         commandPrompt(args);
     }
@@ -75,10 +73,10 @@ public class GameplayUI {
             help();
         }
         else if(command.equalsIgnoreCase("redraw")){
-
+            boardCreation(color);
         }
         else if(command.equalsIgnoreCase("legal")){
-
+            legal();
         }
         else if(command.equalsIgnoreCase("move")){
 
@@ -113,185 +111,55 @@ public class GameplayUI {
         System.out.println("'leave' - this will end your session with us and send you back to the post-login page");
     }
 
-    public static void boardCreation(String color) {
-        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    private static void legal(){
+        //Here we are going to enter the username and password separated by spaces.
+        System.out.print("\u001b[104;1m");
+        System.out.print("\u001b[30;1m");
+        System.out.println("To see what moves are legal by a piece enter the piece position like this (without the dash, or quotation marks):");
+        System.out.print("\u001b[107;1m");
+        System.out.print("\u001b[35;1m");
+        System.out.println("'letter-number'");
 
-        //here we are going to create a board depending on the player side
-        out.print(ERASE_SCREEN);
+        System.out.print("\u001b[49;m");
+        System.out.print("\u001b[33;1m");
+        System.out.printf("[ENTER YOUR PIECE QUERY]>>>");
 
-        if(color.equalsIgnoreCase("WHITE") || color == ""){
-            out.print("    ");
-
-            drawHeaders(out, true);
-
-            drawChessBoard(out, true);
-
-            out.print("    ");
-            drawHeaders(out, true);
-        }
-
-        //here we are going to do an if statement again because if the observer is looking we want to print both
-        if(color.equalsIgnoreCase("BLACK") || color == "") {
-            out.print("    ");
-
-            drawHeaders(out, false);
-
-            drawChessBoard(out, false);
-
-            out.print("    ");
-            drawHeaders(out, false);
-        }
-
-
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
-    }
-
-    private static void drawHeaders(PrintStream out, boolean white) {
-
-        setBlack(out);
-        out.print(" ");
-        String[] whiteHeaders = { "a","b","c","d","e","f","g","h" };
-        String[] blackHeaders = { "h","g","f","e","d","c","b","a" };
-        String[] headers;
-
-        if(white)
-            headers = whiteHeaders;
-        else
-            headers = blackHeaders;
-
-        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headers[boardCol]);
-
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print("  ");
-            }
-        }
-
-        out.println();
-    }
-
-    private static void drawHeader(PrintStream out, String headerText) {
-        printHeaderText(out, headerText);
-    }
-
-    private static void printHeaderText(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
-
-        out.print(player);
-
-        setBlack(out);
-    }
-
-    private static void drawChessBoard(PrintStream out, boolean white) {
-        int startRow = (!white ? 0 : BOARD_SIZE_IN_SQUARES - 1);
-        int endRow = (!white ? BOARD_SIZE_IN_SQUARES : -1);
-        int rowIncrement = (!white ? 1 : -1);
-
-        for (int boardRow = startRow; boardRow != endRow; boardRow += rowIncrement) {
-            colorSwitch(out);
-            drawRowNumbers(out, boardRow + 1, white);
-            out.print(EMPTY);
-            drawRowOfSquares(out, boardRow + 1, white);
-        }
-    }
-
-    private static void drawRowNumbers(PrintStream out, int boardRow, boolean white){
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
-        if(!white)
-            out.print(boardRow);
-        else
-            out.print(abs(boardRow - 9));
-    }
-    private static void drawRowOfSquares(PrintStream out, int rowNum, boolean white) {
-        int startWidth = (!white ? 8 : 1);
-        int endWidth = (!white ? 0 : 9);
-        int widthIncrement = (!white ? -1 : 1);
-
-        for (int squareWidth = startWidth; squareWidth != endWidth; squareWidth += widthIncrement) {
-            colorSwitch(out);
-            if (board.getPiece(new ChessPosition(rowNum, squareWidth)) != null) {
-                ChessPiece piece = board.getPiece(new ChessPosition(rowNum, squareWidth));
-                if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
-                    setMagenta(out);
-                } else {
-                    setGrey(out);
-                }
-
-                switch (piece.getPieceType()) {
-                    case ROOK:
-                        out.print(" R ");
-                        break;
-                    case KNIGHT:
-                        out.print(" N ");
-                        break;
-                    case BISHOP:
-                        out.print(" B ");
-                        break;
-                    case QUEEN:
-                        out.print(" Q ");
-                        break;
-                    case KING:
-                        out.print(" K ");
-                        break;
-                    case PAWN:
-                        out.print(" P ");
-                        break;
-                }
-            } else {
-                out.print(EMPTY);
-            }
-            setBlack(out);
-        }
-
-        out.print(EMPTY);
-        drawRowNumbers(out, rowNum, white);
-        out.println();
-    }
-
-    private static void setWhite(PrintStream out) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_WHITE);
-    }
-
-    private static void setBlue(PrintStream out) {
-        out.print(SET_BG_COLOR_BLUE);
-        out.print(SET_TEXT_COLOR_BLUE);
-    }
-
-    private static void setBlack(PrintStream out) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_BLACK);
-    }
-
-    private static void setGrey(PrintStream out) {
-        out.print(SET_TEXT_COLOR_DARK_GREY);
-    }
-
-    private static void setMagenta(PrintStream out) {
-        out.print(SET_TEXT_COLOR_MAGENTA);
-    }
-
-    private static void colorSwitch(PrintStream out){
-        if(!tileColor){
-            setWhite(out);
-            tileColor = true;
+        Scanner scanner = new Scanner(System.in);
+        char[] input = scanner.nextLine().toCharArray();
+        if(input.length == 2){
+            ChessPosition chessPosition = convertToChessPosition(input);
         }
         else{
-            setBlue(out);
-            tileColor = false;
+            System.out.print("\u001b[31;1m");
+            System.out.println("Make sure you input the right amount of characters dweeb");
         }
-
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
+    /*
+    here we are going to go through each of the characters and depending on which character the player
+    inputted we will convert that to its column number
+    if there is no match we will tell the user to put in a valid input
+    */
+    private static ChessPosition convertToChessPosition(char[] input) {
+        char[] characterList = {'a','b','c','d','e','f','g','h'};
+        int colNum = 0;
+        ChessPosition chessPosition = null;
 
-        out.print(player);
-
-        setWhite(out);
+        for(int i = 0; i < characterList.length; i++){
+            if(input[0] == characterList[i]){
+                colNum = i + 1;
+            }
+        }
+        //if the input is not a character in range or a digit in range
+        if((colNum == 0) || (Integer.parseInt("" + input[1]) < 1 ) || (Integer.parseInt("" + input[1]) > 8 )) {
+            System.out.print("\u001b[31;1m");
+            System.out.println("Make sure your input is correct you dweeb");
+        }
+        else{
+            chessPosition = new ChessPosition(input[1], colNum);
+        }
+        return chessPosition;
     }
+
+
 }
